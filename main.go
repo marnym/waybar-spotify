0e4ab6eb3deb7f79453e4c54032d2e006f5f50e2
Markus Nyman <markus@nyman.dev>
2024-02-06 22:01:21 +0200

    Use bufio.Scanner instead of io.Reader

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 type State struct {
@@ -12,14 +12,17 @@ type State struct {
 	Metadata string `json:"text"`
 }
 
-func listenToCmd(events chan string, cmd *exec.Cmd, b []byte) {
+func listenToCmd(events chan string, cmd *exec.Cmd) {
 	cmdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
 	cmd.Start()
 
-	var n int
-	for err == nil {
-		n, err = cmdOut.Read(b)
-		events <- strings.TrimRight(string(b[:n]), "\n")
+	scanner := bufio.NewScanner(cmdOut)
+	for scanner.Scan() {
+		line := scanner.Text()
+		events <- line
 	}
 	cmd.Wait()
 }
@@ -51,12 +54,8 @@ func main() {
 		}
 	}()
 
-	go listenToCmd(statuses, exec.Command("playerctl", "--player", "spotify", "--follow", "status"), make([]byte, 8))
-	go listenToCmd(
-		metadatas,
-		exec.Command("playerctl", "--player", "spotify", "--follow", "--format", "{{ artist }} - {{ title }}", "metadata"),
-		make([]byte, 64),
-	)
+	go listenToCmd(statuses, exec.Command("playerctl", "--player", "spotify", "--follow", "status"))
+	go listenToCmd(metadatas, exec.Command("playerctl", "--player", "spotify", "--follow", "--format", "{{ artist }} - {{ title }}", "metadata"))
 
 	<-done
 }
