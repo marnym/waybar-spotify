d1277e4364cdd2c20e41d903507809f18c9fabb7
Markus Nyman <markus@nyman.dev>
2025-09-21 13:46:34 +0300

    Epoll based implementation
    Not a necessary change. Mostly done for experimental reasons. One
    positive outcome is that we got rid of the mutex.

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const posix = std.posix;
+const os = std.os.linux;
 const ChildProcess = std.process.Child;
 
 pub const Listener = enum {
@@ -23,9 +25,6 @@ pub const State = struct {
     /// contains information of current track in the form of `Artist - Song`
     metadata: []const u8,
 
-    writer: *std.Io.Writer,
-    mutex: std.Thread.Mutex,
-
     pub fn jsonStringify(value: State, jws: anytype) !void {
         try jws.beginObject();
         try jws.objectField("class");
@@ -38,64 +37,69 @@ pub const State = struct {
 
 const base_args = [_][]const u8{ "playerctl", "--player", "spotify", "--follow" };
 
-pub fn createProcess(comptime args: []const []const u8, allocator: std.mem.Allocator) ChildProcess {
+/// Creates and spawns a process.
+pub fn createProcess(comptime args: []const []const u8, allocator: std.mem.Allocator) !ChildProcess {
     var process = ChildProcess.init(base_args ++ args, allocator);
     process.stdout_behavior = .Pipe;
     process.stderr_behavior = .Ignore;
     process.stdin_behavior = .Ignore;
+
+    try process.spawn();
+
     return process;
 }
 
-pub fn listenToProcess(comptime listener: Listener, state: *State, allocator: std.mem.Allocator) !void {
-    const args = comptime listener.args();
-    var process = createProcess(args, allocator);
-    process.spawn() catch |err| {
-        const joined_args = std.mem.join(allocator, " ", process.argv) catch "ALLOCATION_FAILED";
-        std.debug.print("failed to spawn {s}: {}\n", .{ joined_args, err });
-        return;
-    };
-    defer {
-        _ = process.kill() catch |err| {
-            const joined_args = std.mem.join(allocator, " ", process.argv) catch "ALLOCATION_FAILED";
-            std.debug.print("failed to kill {s}: {}\n", .{ joined_args, err });
-        };
-    }
-
-    var msg_buf: [256]u8 = undefined;
-    var reader = process.stdout.?.reader(&msg_buf);
-    while (reader.interface.takeDelimiterExclusive('\n')) |msg| {
-        const trimmed = std.mem.trimRight(u8, msg, "\n");
-
-        state.mutex.lock();
-        defer state.mutex.unlock();
-        switch (listener) {
-            .Metadata => state.metadata = trimmed,
-            .Status => state.playing = std.mem.eql(u8, "Playing", msg),
-        }
-        try std.json.fmt(state, .{}).format(state.writer);
-        try state.writer.writeByte('\n');
-        try state.writer.flush();
-    } else |err| switch (err) {
-        error.EndOfStream => {},
-        error.StreamTooLong => std.debug.print("line is too long for buffer: {}\n", .{err}),
-        else => std.debug.print("unexpected error: {}\n", .{err}),
-    }
+pub fn listenToProcess(process: ChildProcess, efd: i32) !void {
+    const fd = process.stdout.?.handle;
+    var event = os.epoll_event{ .events = posix.POLL.IN, .data = .{ .fd = fd } };
+    try posix.epoll_ctl(efd, os.EPOLL.CTL_ADD, fd, &event);
 }
 
 pub fn main() !void {
     const stdout = std.fs.File.stdout();
     var stdout_buffer: [1024]u8 = undefined;
     var writer = stdout.writer(&stdout_buffer);
-    var state = State{ .playing = false, .metadata = "", .writer = &writer.interface, .mutex = std.Thread.Mutex{} };
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    {
-        const status_thread = try std.Thread.spawn(.{}, listenToProcess, .{ Listener.Status, &state, allocator });
-        defer status_thread.join();
+    const efd = try posix.epoll_create1(os.EPOLL.CLOEXEC);
+    defer posix.close(efd);
 
-        const metadata_thread = try std.Thread.spawn(.{}, listenToProcess, .{ Listener.Metadata, &state, allocator });
-        defer metadata_thread.join();
+    const process_status = createProcess(Listener.Status.args(), allocator) catch |err|
+        return std.debug.print("failed to spawn {s}: {}\n", .{ std.enums.tagName(Listener, Listener.Status).?, err });
+    const handle_status = process_status.stdout.?.handle;
+
+    const process_metadata = createProcess(Listener.Metadata.args(), allocator) catch |err|
+        return std.debug.print("failed to spawn {s}: {}\n", .{ std.enums.tagName(Listener, .Metadata).?, err });
+    const handle_metadata = process_metadata.stdout.?.handle;
+
+    try listenToProcess(process_status, efd);
+    try listenToProcess(process_metadata, efd);
+
+    var state = State{
+        .playing = false,
+        .metadata = "",
+    };
+
+    var ready_list: [8]os.epoll_event = undefined;
+    while (true) {
+        const ready_count = posix.epoll_wait(efd, &ready_list, -1);
+        for (ready_list[0..ready_count]) |ready| {
+            const ready_fd = ready.data.fd;
+            var buf: [256]u8 = undefined;
+            const read = posix.read(ready_fd, &buf) catch 0;
+            const output = std.mem.trimEnd(u8, buf[0..read], "\n");
+
+            if (ready_fd == handle_status) {
+                state.playing = std.mem.eql(u8, "Playing", output);
+            } else if (ready_fd == handle_metadata) {
+                allocator.free(state.metadata);
+                state.metadata = try allocator.dupe(u8, output);
+            }
+        }
+        try std.json.fmt(state, .{}).format(&writer.interface);
+        try writer.interface.writeByte('\n');
+        try writer.interface.flush();
     }
 }
